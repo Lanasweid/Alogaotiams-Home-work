22a3f413657882440fb76d7adc68f1d898a55fc1
Administrator <Administrator@PC>
2024-10-30 09:27:16 +0100

    Add project files.

---
+++ Alogratims home work/BinarySearchExample.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alogratims_home_work
+{
+    class BinarySearchExample
+    {
+        public static int IterativeBinary(int[] array, int target)
+        {
+
+            int left = 0;
+            int right = array.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                //تحقق اذا كانت القيمة في المنصف هي الهدف 
+                if (array[mid] == target)
+                {
+                    return mid;
+
+                }
+                //اذا كانت القيمة الهدف اكبر تجاهل الجزء الايسر
+                else if (array[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                //اذا كانت القيمة الهدف اصغر تجاهل الجزء الايمن 
+                else
+                {
+                    right = mid - 1;
+                    {
+                        {
+                            return -1;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+        class RecursiveBinarySearch
+        { 
+            public static int RecursiveBinarySearch(int[] array, int target, int left, int right)
+            {
+                if (left > right)
+                {
+                    return -1;
+                }
+                int mid = left + (right - left) / 2;
+                if (array[mid] == target)
+                {
+                    return mid;
+
+                }
+                else if (array[mid] < target)
+                {
+                    return RecursiveBinarySearch(array, target, mid + 1, right);
+                }
+                else
+                {
+                    return RecursiveBinarySearch(array, target, left, mid - 1);
+                }
+
+            }
+        }
+        
+
+
+
+
+
+
+
+
+            
+          
+
+            
+
+           
+       
+            
+            
+            
+            
+
+
+
+    
---
+++ Alogratims home work/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alogratims_home_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+            int target = 7;
+            int iterativeresult = IterativeBinary(array, target);
+            if (iterativeresult != -1)
+            {
+                Console.WriteLine($"value {target}(iterative)is found in the index:{iterativeresult}");
+            }
+            else
+            {
+                Console.WriteLine($"value{target}(iterative)is not found");
+            }
+
+            int recursiveresult = RecursiveBinarySearch(array, target, 0, array.Length - 1);
+            if (recursiveresult != -1)
+            {
+                Console.WriteLine($"value{target}(recursive) is found in index:{recursiveresult}");
+            }
+            else
+            {
+                Console.WriteLine($"value{target}(recursive) is not found");
+            }
+        }
+
+        private static int RecursiveBinarySearch(int[] array, int target, int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int IterativeBinary(int[] array, int target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
